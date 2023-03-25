f1c5447cd32ea8b1f282941887c4599c9b368087
Jodi Oo <bolshack_fire@hotmail.com>
2023-03-26 03:42:59 +0800

    Add logic to sanitize inputs for mongodb nosql injection

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 const express = require('express')
+const { mongoDBNoSQLMatch } = require('./DetectionFunction')
 
-const injectionSanitizer = () => {
+const validateRequestBody = (requestBody, requestBodyPathsToSanitize, res) => {
+  const result =  {}
+  for (requestBodyPathToSanitize of requestBodyPathsToSanitize) {
+    let input = requestBody
+    requestBodyPathToSanitize.split('.')
+    .forEach((p) => { input = input[p]; });
+    if (mongoDBNoSQLMatch(input)) {
+      result['shouldThrowError'] = true
+    }
+  }
+  return result
+}
+
+const validateGenericParam = (param, paramsToSanitize) => {
+  const result = {}
+  for (paramToSanitize of paramsToSanitize) {
+    if (mongoDBNoSQLMatch(param[paramToSanitize])) {
+      result['shouldThrowError'] = true
+    }
+  }
+  return result
+}
+
+const injectionSanitizer = (args = {}) => {
+  const {
+    headersToSanitize = [],
+    requestBodyPathsToSanitize = [],
+    requestParamsToSanitize = [],
+    queryParamsToSanitize = [],
+  } = args
   return (req, res, next) => {
     express.json()(req, res, () => {
-      const {headers: requestHeaders, body: requestBody, params: requestParams, query: queryParams} = req
-      console.log("Headers")
-      console.log(requestHeaders)
-      console.log("Body")
-      console.log(requestBody)
-      console.log("Request Params")
-      console.log(requestParams)
-      console.log("Request Query Params")
-      console.log(queryParams)
+
+      const {
+        headers: requestHeaders,
+        body: requestBody,
+        params: requestParams,
+        query: queryParams
+      } = req
+
+      const {
+        shouldThrowError: shouldThrowErrorAfterValidatingHeaders = false
+      } = validateGenericParam(requestHeaders, headersToSanitize, res)
+      if (shouldThrowErrorAfterValidatingHeaders) {
+        return res.sendStatus(500)
+      }
+
+      const {
+        shouldThrowError: shouldThrowErrorAfterValidatingRequestBody = false
+      } = validateRequestBody(requestBody, requestBodyPathsToSanitize, res)
+      if (shouldThrowErrorAfterValidatingRequestBody) {
+        return res.sendStatus(500)
+      }
+
+      const {
+        shouldThrowError: shouldThrowErrorAfterValidatingRequestParams = false
+      } = validateGenericParam(requestParams, requestParamsToSanitize, res)
+      if (shouldThrowErrorAfterValidatingRequestParams) {
+        return res.sendStatus(500)
+      }
+
+      const {
+        shouldThrowError: shouldThrowErrorAfterValidatingQueryParams = false
+      } = validateGenericParam(queryParams,queryParamsToSanitize, res)
+      if (shouldThrowErrorAfterValidatingQueryParams) {
+        return res.sendStatus(500)
+      }
+
       next()
     })
   }
