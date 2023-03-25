85594fc7aa9ef0d7924c760cffd761155cc20d1f
Jodi Oo <bolshack_fire@hotmail.com>
2023-03-25 17:46:50 +0800

    Make changes so that request headers, request body, request params and query params are readable

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const injectionSanitizer = (req, res, next) => {
-  console.log('Hello World')
-  console.log(req)
-  next()
+const express = require('express')
+
+const injectionSanitizer = () => {
+  return (req, res, next) => {
+    express.json()(req, res, () => {
+      const {headers: requestHeaders, body: requestBody, params: requestParams, query: queryParams} = req
+      console.log("Headers")
+      console.log(requestHeaders)
+      console.log("Body")
+      console.log(requestBody)
+      console.log("Request Params")
+      console.log(requestParams)
+      console.log("Request Query Params")
+      console.log(queryParams)
+      next()
+    })
+  }
 }
 
 module.exports = injectionSanitizer
